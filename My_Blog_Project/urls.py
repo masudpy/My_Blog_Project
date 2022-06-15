bb061409b700d5ee8b5a35a94f33613712d4bb81
masudpy <masudcse74@gmail.com>
2022-06-15 16:37:32 +0600

    Django Project Done!

---
+++ My_Blog_Project/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('account/', include('App_Login.urls')),
+    path('blog/', include('App_Blog.urls')),
+    path('', views.Index, name='index'),
+]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
